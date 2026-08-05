1bd156573ae8c9420fc456d8a209125bca341737
MederbekTuratbekov <142796921+MederbekTuratbekov@users.noreply.github.com>
2026-08-05 15:08:29 +0600

    ...

--- fastapi_wildberries/api/cart.py
+++ fastapi_wildberries/api/cart.py
@@ -1,7 +1,7 @@
 from fastapi import Depends, HTTPException, APIRouter
-from store_app.db.models import Cart, CartItem, Product
-from store_app.db.schema import CartCreateSchema, CartSchema # CartItemSchema
-from store_app.db.database import SessionLocal
+from fastapi_wildberries.db.models import Cart, CartItem, Product
+from fastapi_wildberries.db.schema import CartCreateSchema, CartSchema
+from fastapi_wildberries.db.database import SessionLocal
 from sqlalchemy.orm import Session
 
 
@@ -16,23 +16,23 @@ def get_db():
 
 @cart_router.get('/', response_model=CartSchema)
 async def cart_list(user_id: int, db: Session = Depends(get_db)):
-    cart = db.query(Cart).filter(Cart.user_id == user_id).first() # есть ли корзина пользователья (по этому id) в базе данных?
+    cart = db.query(Cart).filter(Cart.user_id == user_id).first()
     if not cart:
         raise HTTPException(status_code=404, detail=f'Корзина для пользователя с ID {user_id} не найдена в базе данных')
     return cart
 
 @cart_router.post('/', response_model=CartCreateSchema)
 async def cart_add(item_data: CartCreateSchema, user_id: int, db: Session = Depends(get_db)):
-    cart = db.query(Cart).filter(Cart.user_id == user_id).first() # корзинасы барбы?
+    cart = db.query(Cart).filter(Cart.user_id == user_id).first()
     if not cart:
         cart = Cart(user_id=user_id)
         db.add(cart)
         db.commit()
         db.refresh(cart)
-    product = db.query(Product).filter(Product.id == item_data.product_id).first() # есть ли такой продукт в базе данных?
+    product = db.query(Product).filter(Product.id == item_data.product_id).first()
     if not product:
-        raise HTTPException(status_code=404, detail='Мындай продукт жок')
-    product_item = db.query(CartItem).filter(CartItem.cart_id == cart.id, CartItem.product_id == item_data.product_id).first() # есть ли в конкретной корзине, определённый продукт?
+        raise HTTPException(status_code=404, detail='Продукт не найден')
+    product_item = db.query(CartItem).filter(CartItem.cart_id == cart.id, CartItem.product_id == item_data.product_id).first()
     if product_item:
         raise HTTPException(status_code=400, detail='Продукт уже в корзине')
     cart_item = CartItem(cart_id=cart.id, product_id=item_data.product_id)
@@ -45,10 +45,10 @@ async def cart_add(item_data: CartCreateSchema, user_id: int, db: Session = Depe
 async def cart_delete(product_id: int, user_id: int, db: Session = Depends(get_db)):
     cart = db.query(Cart).filter(Cart.user_id == user_id).first()
     if not cart:
-        raise HTTPException(status_code=404, detail='база данныхта корзинасы жок')
-    cart_item = db.query(CartItem).filter(CartItem.cart_id == cart.id, CartItem.product_id == product_id).first() # есть ли в конкретной корзине, определённый продукт?
+        raise HTTPException(status_code=404, detail='Корзина в базе данных не найдена')
+    cart_item = db.query(CartItem).filter(CartItem.cart_id == cart.id, CartItem.product_id == product_id).first()
     if not cart_item:
-        raise HTTPException(status_code=404, detail='Корзинада мындай продукт жок')
+        raise HTTPException(status_code=404, detail='В корзине нет такого продукта')
     db.delete(cart_item)
     db.commit()
     return {'message': 'Cart item deleted successfully'}
